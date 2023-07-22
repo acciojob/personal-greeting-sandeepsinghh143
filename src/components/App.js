5c4aaae2173b53f7079b829e2fc4f014772bde5a
sandeepsinghh143 <sandeepsinghh143@gmail.com>
2023-07-22 13:31:22 +0530

    submitting code-Sat Jul 22 2023 13:31:20 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [text, setText]=useState("");
+  function greet(event){
+    setText(event.target.value);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <label>Enter your Name:</label><br/>
+        <input type="text" onChange={greet}/>
+        {text && <p>Hello {text}</p>}
     </div>
   )
 }
